bb845a89cd6f3df874eeede4711703206cb87373
Aidin Gharibnavaz <aidin@aidinhut.com>
2016-06-26 21:51:17 +0430

    Checking the result wasn't correct.

--- tests/test_json_listener/test_method.py
+++ tests/test_json_listener/test_method.py
@@ -16,18 +16,22 @@
 # <http://www.gnu.org/licenses/>.
 
 import unittest
+import json
 
 import json_listener
 
 
 class TestMethod(unittest.TestCase):
 
-    def fake_start_response(self):
+    def fake_start_response(self, *args, **kwargs):
         pass
 
     def test_invalid_method(self):
         '''Sends invalid method to the listener.'''
         env = {"REQUEST_METHOD": "GET"}
 
-        with self.assertRaises(json_listener.InvalidHttpMethodError):
-            json_listener.application(env, self.fake_start_response)
+        json_result = json_listener.application(env, self.fake_start_response)
+        result = json.loads(json_result[0].decode('utf-8'))
+
+        self.assertEqual(result['status'], 500)
+        self.assertEqual(result['error_code'], 'InvalidHttpMethodError')
