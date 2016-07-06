f7e81ff70f1da13ad4ed251cbf8369c072f4cd55
Aidin Gharibnavaz <aidin@aidinhut.com>
2016-07-06 20:40:39 +0430

    Adding test born with a parent.

--- tests/test_population_control/test_born.py
+++ tests/test_population_control/test_born.py
@@ -17,6 +17,8 @@
 
 import unittest
 
+from actions.exceptions import InvalidArgumentsError
+from objects.robot import Robot
 from population.population_control import PopulationControl
 from database.memcached_database import MemcachedDatabase
 from database.exceptions import InvalidPasswordError
@@ -31,7 +33,7 @@ class TestBorn(unittest.TestCase):
 
         database.add_password("iujeh87UYh6512ewQ")
 
-        robot_info = population_control.execute_command("iujeh87UYh6512ewQ", "born", [])
+        robot_info = population_control.execute_command("iujeh87UYh6512ewQ", "born", [None, "RDaniel"])
         database.commit()
 
         database.get_robot(robot_info['robot_id'])
@@ -43,5 +45,26 @@ class TestBorn(unittest.TestCase):
         with self.assertRaises(InvalidPasswordError):
             population_control.execute_command("wrong_pass_1273", "born", [])
 
-    # TODO: Add this test when world completed.
-#    def test_with_parent(self):
+    def test_bad_parent_id(self):
+        '''Tests with an invalid parent ID.'''
+        population_control = PopulationControl()
+
+        with self.assertRaises(InvalidArgumentsError):
+            population_control.execute_command("123", "born", [1334])
+
+        with self.assertRaises(InvalidArgumentsError):
+            population_control.execute_command("123", "born", [b"some bytes"])
+
+    def test_with_parent(self):
+        '''Tests borning a robot with a parent.'''
+        population_control = PopulationControl()
+        database = MemcachedDatabase()
+
+        database.add_password("oijdnnh76153WEd")
+        robot = Robot("test_with_parent_18873", "123")
+        database.add_robot(robot, 14, 1)
+        database.commit()
+
+        population_control.execute_command("oijdnnh76153WEd", "born", ["test_with_parent_18873", "My Child"])
+
+        database.rollback()
