def8103eb0d4bb31c0e70e00a682e41f615252ee
Aidin Gharibnavaz <aidin@aidinhut.com>
2016-07-09 08:19:42 +0430

    Better NewWorld class. Now it creates a fresh instance of all the classes.

--- tests/test_world/test_load_from_file.py
+++ tests/test_world/test_load_from_file.py
@@ -23,25 +23,18 @@ import random
 import subprocess
 
 from database.memcached_connection import MemcachedConnection
+from database.memcached_database import MemcachedDatabase
 from world.world import World
 from world.exceptions import InvalidWorldFileError
 from utils.configs import Configs
 
+
 class NewWorld:
-    '''Forces the world to create a new instance, instead of
-    beign a singleton.
+    '''Creates a fresh server, by creating new instances of World and MemcachedDatabase,
+    and starting a new and empty Memcached database.
     '''
+
     def __enter__(self):
-        self._original_new = World.__new__
-        World.__new__ = object.__new__
-
-    def __exit__(self, *args):
-        World.__new__ = self._original_new
-
-
-class TestLoadFromFile(unittest.TestCase):
-
-    def setUp(self):
         # Starting a new database on a random port.
         port = random.randrange(11600, 11700)
         self._memcached_process = subprocess.Popen(["memcached", "-l", "127.0.0.1", "-p", str(port)])
@@ -49,29 +42,46 @@ class TestLoadFromFile(unittest.TestCase):
         # Waiting for the memcached to start.
         time.sleep(0.5)
 
+        # Forcing the world to create a new instance.
+        self._original_world_instance = World._single_instance
+        World._single_instance = None
+
+        # Forcing the database to create a new instance.
+        self._original_database_instance = MemcachedDatabase._single_instance
+        MemcachedDatabase._single_instance = None
+
+        # Initializing the database.
+        MemcachedDatabase().initialize()
+
+        # Configuring connection to use this new port.
         mc_connection = MemcachedConnection()
         mc_connection.config_connection(port)
 
-    def tearDown(self):
-        # Resetting database to its default port.
-        MemcachedConnection().config_connection(Configs().get_database_port())
 
+    def __exit__(self, *args):
+        # Setting back everything to their original values.
+        World._single_instance = self._original_world_instance
+        MemcachedDatabase._single_instance = self._original_database_instance
+        MemcachedConnection().config_connection(Configs().get_database_port())
         self._memcached_process.terminate()
+
+
+class TestLoadFromFile(unittest.TestCase):
 
     def test_invalid_length(self):
         '''Tests with a file that one of it's row's length is invalid.'''
 
-        map_data = ("00122\n"
-                    "11332\n"
-                    "123000\n"
-                    "00112\n")
+        with NewWorld():
+            map_data = ("00122\n"
+                        "11332\n"
+                        "123000\n"
+                        "00112\n")
 
-        temp_map_fd, temp_map_file_path = tempfile.mkstemp()
-        os.write(temp_map_fd, map_data.encode('utf-8'))
-        os.close(temp_map_fd)
+            temp_map_fd, temp_map_file_path = tempfile.mkstemp()
+            os.write(temp_map_fd, map_data.encode('utf-8'))
+            os.close(temp_map_fd)
 
-        with self.assertRaises(InvalidWorldFileError):
-            with NewWorld():
+            with self.assertRaises(InvalidWorldFileError):
                 world = World()
                 world.load_from_file(temp_map_file_path)
 
@@ -83,16 +93,16 @@ class TestLoadFromFile(unittest.TestCase):
 
     def test_invalid_square(self):
         '''Tries to load a file which contains an invalid square number.'''
-        map_data = ("00122\n"
-                    "11332\n"
-                    "12400\n"
-                    "00112\n")
+        with NewWorld():
+            map_data = ("00122\n"
+                        "11332\n"
+                        "12400\n"
+                        "00112\n")
 
-        temp_map_fd, temp_map_file_path = tempfile.mkstemp()
-        os.write(temp_map_fd, map_data.encode('utf-8'))
-        os.close(temp_map_fd)
+            temp_map_fd, temp_map_file_path = tempfile.mkstemp()
+            os.write(temp_map_fd, map_data.encode('utf-8'))
+            os.close(temp_map_fd)
 
-        with self.assertRaises(InvalidWorldFileError):
-            with NewWorld():
+            with self.assertRaises(InvalidWorldFileError):
                 world = World()
                 world.load_from_file(temp_map_file_path)
